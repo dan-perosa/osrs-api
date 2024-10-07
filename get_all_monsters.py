38b30e871cd4d05e44b5d173b3faa3d9f7060dc4
Daniel <daniel.perosa@desbravador.com.br>
2024-10-07 17:18:44 -0300

    Fixed get_all_monsters endpoint

--- get_all_monsters.py
+++ get_all_monsters.py
@@ -3,27 +3,36 @@ from models import Monster
 from sqlalchemy import select
 from flask import jsonify
 
+def take_comma_and_transform(result):
+    to_return = result
+    if ',' in result:
+        to_return = result.replace(',', '')
+    
+    return int(to_return)
+        
+
 def get_all_monsters():
     q = select(Monster)
     results = session.execute(q).scalars().all()
     monsters = []
     for result in results:
         monsters.append({
-            'ID': result.id,
-            'Image': result.image,
-            'Monster Name': result.monster_name,
-            'Members': result.members,
-            'Combat Level': result.combat_level,
-            'Hitpoints': result.hitpoints,
-            'Attack Level': result.attack_level,
-            'Defence Level': result.defence_level,
-            'Magic Level': result.magic_level,
-            'Ranged Level': result.ranged_level,
-            'Stab Defence': result.stab_defence,
-            'Crush Defence': result.crush_defence,
-            'Magic Defence': result.magic_defence,
-            'Light Ranged Defence': result.light_ranged_defence,
-            'Standard Ranged Defence': result.standard_ranged_defence,
-            'Heavy Ranged Defence': result.heavy_ranged_defence,
+            'id': result.id,
+            'image': result.image,
+            'monster_name': result.monster_name,
+            'members': result.members,
+            'combat_level': take_comma_and_transform(result.combat_level),
+            'hitpoints': take_comma_and_transform(result.hitpoints),
+            'attack_level': take_comma_and_transform(result.attack_level),
+            'defence_level': take_comma_and_transform(result.defence_level),
+            'magic_level': take_comma_and_transform(result.magic_level),
+            'ranged_level': take_comma_and_transform(result.ranged_level),
+            'stab_defence': take_comma_and_transform(result.stab_defence),
+            'slash_defence': take_comma_and_transform(result.slash_defence),
+            'crush_defence': take_comma_and_transform(result.crush_defence),
+            'magic_defence': take_comma_and_transform(result.magic_defence),
+            'light_ranged_defence': take_comma_and_transform(result.light_ranged_defence),
+            'standard_ranged_defence': take_comma_and_transform(result.standard_ranged_defence),
+            'heavy_ranged_defence': take_comma_and_transform(result.heavy_ranged_defence),
         })
     return jsonify(monsters)
