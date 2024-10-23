1e3912981c9f5cc3d7c7985f5a80dac3d51161e8
Daniel <daniel.perosa@desbravador.com.br>
2024-10-23 17:26:33 -0300

    Started adding complete list to database

--- user_endpoints/sum_score.py
+++ user_endpoints/sum_score.py
@@ -2,8 +2,10 @@ from db_conn import session
 from models import User
 from flask import jsonify
 from sqlalchemy import select, update
-from werkzeug.security import check_password_hash
 import jwt
+import datetime
+import json
+import pprint
 
 jwt_key = 'secret'
 
@@ -11,6 +13,7 @@ def sum_score(user_info):
     score = user_info['score']
     token = user_info['token']
     full_list = user_info['list']
+    minigame = user_info['minigame']
     username = jwt.decode(token, jwt_key, algorithms='HS256')['user']
     
     q = select(User).where(User.username == username)
@@ -20,10 +23,17 @@ def sum_score(user_info):
     
     previous_score = results[0].score
     new_score = previous_score + score
-
-    q = update(User).where(User.username == username).values(score=new_score)
-    session.execute(q)
-    session.commit()
+    
+    q = select(User).where(User.username == username)
+    results = session.execute(q).scalars().all()
+    list_of_completed_minigames = results[0].finished_info
+    print(json.dumps(list_of_completed_minigames))
+    dumped = json.dumps(list_of_completed_minigames)
+    loaded = json.loads(dumped)
+    pprint.pprint(loaded)
+    # q = update(User).where(User.username == username).values(score=new_score)
+    # session.execute(q)
+    # session.commit()
 
     return jsonify({'message': 'points_updated'})
 
